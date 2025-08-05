33ab4364ac01e56278acbb656ed257668c7080d3
Prathamesh <prathameshdevkar1998@gmail.com>
2025-08-05 14:53:00 +0530

    cicd run 1.7

--- src/data_components/data_ingestion.py
+++ src/data_components/data_ingestion.py
@@ -79,7 +79,7 @@ def main():
         # test_size = 0.2
         
         # df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
-        df=pd.read_csv(r"C:\Users\Prathamesh\MLOPS\mlops_capstone_project_movie_recommender_system\notebooks\data.csv")
+        df=pd.read_csv(r"data\data.csv")
         # s3 = s3_connection.s3_operations("bucket-name", "accesskey", "secretkey")
         # df = s3.fetch_file_from_s3("data.csv")
 
